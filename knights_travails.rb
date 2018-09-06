94a503c8438f81dbe8bb5b7a889f1626588f4fd6
Alan Vardy <alanvardy@gmail.com>
2018-09-05 19:57:56 -0600

    Fixed Node init issues

--- knights_travails.rb
+++ knights_travails.rb
@@ -5,9 +5,15 @@ class ChessBoard
 end
 
 class Node
+  attr_accessor :x, :y, :path
   def initialize(x, y, path = [])
     @x = x
     @y = y
     @path = path << [x, y]
   end
 end
+
+test = Node.new(1, 3)
+puts test.path.inspect
+test2 = Node.new(1, 5, test.path)
+puts test2.path.inspect
