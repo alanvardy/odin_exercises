eef375697363d72e7ca1ea55ab0b793c1ec1a37e
Alan Vardy <alanvardy@gmail.com>
2018-09-06 14:38:38 -0600

    Added on_board method to node class

--- knights_travails.rb
+++ knights_travails.rb
@@ -4,11 +4,12 @@ class ChessBoard
     @found = false
   end
 
-  def create_children(node) #todo something is seriously wrong here
+  def create_children(node)
     moves = [{x: 2, y: 1}, {x: 2, y: -1}, {x: -2, y: 1}, {x: -2, y: -1}, {x: 1, y: 2}, {x: 1, y: -2}, {x: -1, y: 2}, {x: -1, y: -2}]
     moves.each do |move|
-      @queue << Node.new(node.x + move[:x], node.y + move[:y], node.path)
-      puts @queue[-1].inspect
+      new_location = Node.new(node.x + move[:x], node.y + move[:y])
+      @queue << new_location if new_location.on_board?
+      puts @queue[-1].inspect if new_location.on_board? #todo remove line (testing)
     end
   end
 
@@ -29,13 +30,16 @@ end
 
 class Node
   attr_accessor :x, :y
-  attr_reader :path
 
-  def initialize(x, y, path_init = [])
+  def initialize(x, y)
     @x = x
     @y = y
-    @path = path_init << [x, y]
   end
+
+  def on_board?
+    return (@x < 9 && @x > 0 && @y < 9 && @y > 0) ? true : false
+  end
+
 end
 
 board = ChessBoard.new
